e434a1d501ed92ae9bf2dc808e2bb54479b90c9f
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-10 16:45:01 +0530

    Concept of Delegates and events

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+delegate int NumberChanger(int value);
+
+namespace DelegatesAndEvents
+{
+    internal class Program
+    {
+        static int number = 5;
+        static void Main(string[] args)
+        {
+            NumberChanger numberOne = new NumberChanger(AddNumber);
+            NumberChanger numberTwo = new NumberChanger(MultiplyNumber);
+            numberOne(4);
+            Console.WriteLine(GetNumber());
+            numberTwo(6);
+            Console.WriteLine(GetNumber());
+            Console.ReadLine();
+
+        }
+        public static int AddNumber(int X)
+        {
+             number+=X;
+            return number;
+        }
+
+        public static int MultiplyNumber(int Y)
+        {
+            number *= Y;
+            return number;
+        }
+
+        public static int GetNumber()
+        {
+            return number;
+        }
+    }
+}
